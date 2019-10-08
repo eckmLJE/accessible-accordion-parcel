6994447aca71033bc388111693010801894a12b7
Lucas Eckman <lucas.eckman@gmail.com>
2019-10-08 15:06:53 -0400

    importing sample data

--- index.js
+++ index.js
@@ -1,32 +1,4 @@
-const sampleAccData = {
-  items: [
-    {
-      title: 'Item 1',
-      content:
-        'Lorem ipsum dolor amet cloud bread fashion axe actually chia four dollar toast, ramps photo booth <a href="#" >drinking vinegar</a>.',
-    },
-    {
-      title: 'Item 2',
-      content:
-        'Bitters tofu ennui microdosing butcher, biodiesel VHS single-origin coffee ethical polaroid organic. Pug ethical health goth tote bag vape authentic. Copper mug cornhole try-hard fanny pack chia polaroid hell of coloring book everyday carry yuccie cloud bread pour-over hashtag craft beer disrupt.',
-    },
-    {
-      title: 'Item 3',
-      content:
-        "Wayfarers kombucha woke, organic tumblr dreamcatcher yr DIY sartorial brunch actually. Master cleanse ugh crucifix palo santo heirloom put a bird on it. Echo park taxidermy asymmetrical small batch intelligentsia ethical coloring book cornhole. Hella edison bulb disrupt tilde franzen, yr sustainable. Vaporware YOLO hammock keytar, PBR&B squid you probably haven't heard of them. Tofu taiyaki literally, drinking vinegar farm-to-table 90's PBR&B put a bird on it small batch ugh retro.",
-    },
-    {
-      title: 'Item 4',
-      content:
-        "Wayfarers kombucha woke, organic tumblr dreamcatcher yr DIY sartorial brunch actually. Master cleanse ugh crucifix palo santo heirloom put a bird on it. Echo park taxidermy asymmetrical small batch intelligentsia ethical coloring book cornhole. Hella edison bulb disrupt tilde franzen, yr sustainable. Vaporware YOLO hammock keytar, PBR&B squid you probably haven't heard of them. Tofu taiyaki literally, drinking vinegar farm-to-table 90's PBR&B put a bird on it small batch ugh retro.",
-    },
-    {
-      title: 'Item 5',
-      content:
-        "Wayfarers kombucha woke, organic tumblr dreamcatcher yr DIY sartorial brunch actually. Master cleanse ugh crucifix palo santo heirloom put a bird on it. Echo park taxidermy asymmetrical small batch intelligentsia ethical coloring book cornhole. Hella edison bulb disrupt tilde franzen, yr sustainable. Vaporware YOLO hammock keytar, PBR&B squid you probably haven't heard of them. Tofu taiyaki literally, drinking vinegar farm-to-table 90's PBR&B put a bird on it small batch ugh retro.",
-    },
-  ],
-}
+import sampleAccData from './sample-data'
 
 const buildAccordion = accData => {
   const itemsHtmlString = accData.items.map(mapAccordionItems).join('')
@@ -67,14 +39,16 @@ const getBody = num =>
     `.accordion-item[data-accitem="${num}"] .accordion-body`
   )
 
+let currentAccordion = null
+
 const handleAccordionClick = e => {
   const clickedAccordionNum = getItemNum(e)
-  if (this.currentAccordion === clickedAccordionNum) {
+  if (currentAccordion === clickedAccordionNum) {
     const body = getBody(clickedAccordionNum)
     shrinkAccordion(body)
-    this.currentAccordion = null
+    currentAccordion = null
   } else {
-    this.currentAccordion = clickedAccordionNum
+    currentAccordion = clickedAccordionNum
     refreshAccordion(clickedAccordionNum)
   }
 }
---
+++ sample-data.js
@@ -0,0 +1,29 @@
+export default {
+    items: [
+      {
+        title: 'Item 1',
+        content:
+          'Lorem ipsum dolor amet cloud bread fashion axe actually chia four dollar toast, ramps photo booth <a href="#" >drinking vinegar</a>.',
+      },
+      {
+        title: 'Item 2',
+        content:
+          'Bitters tofu ennui microdosing butcher, biodiesel VHS single-origin coffee ethical polaroid organic. Pug ethical health goth tote bag vape authentic. Copper mug cornhole try-hard fanny pack chia polaroid hell of coloring book everyday carry yuccie cloud bread pour-over hashtag craft beer disrupt.',
+      },
+      {
+        title: 'Item 3',
+        content:
+          "Wayfarers kombucha woke, organic tumblr dreamcatcher yr DIY sartorial brunch actually. Master cleanse ugh crucifix palo santo heirloom put a bird on it. Echo park taxidermy asymmetrical small batch intelligentsia ethical coloring book cornhole. Hella edison bulb disrupt tilde franzen, yr sustainable. Vaporware YOLO hammock keytar, PBR&B squid you probably haven't heard of them. Tofu taiyaki literally, drinking vinegar farm-to-table 90's PBR&B put a bird on it small batch ugh retro.",
+      },
+      {
+        title: 'Item 4',
+        content:
+          "Wayfarers kombucha woke, organic tumblr dreamcatcher yr DIY sartorial brunch actually. Master cleanse ugh crucifix palo santo heirloom put a bird on it. Echo park taxidermy asymmetrical small batch intelligentsia ethical coloring book cornhole. Hella edison bulb disrupt tilde franzen, yr sustainable. Vaporware YOLO hammock keytar, PBR&B squid you probably haven't heard of them. Tofu taiyaki literally, drinking vinegar farm-to-table 90's PBR&B put a bird on it small batch ugh retro.",
+      },
+      {
+        title: 'Item 5',
+        content:
+          "Wayfarers kombucha woke, organic tumblr dreamcatcher yr DIY sartorial brunch actually. Master cleanse ugh crucifix palo santo heirloom put a bird on it. Echo park taxidermy asymmetrical small batch intelligentsia ethical coloring book cornhole. Hella edison bulb disrupt tilde franzen, yr sustainable. Vaporware YOLO hammock keytar, PBR&B squid you probably haven't heard of them. Tofu taiyaki literally, drinking vinegar farm-to-table 90's PBR&B put a bird on it small batch ugh retro.",
+      },
+    ],
+  }
